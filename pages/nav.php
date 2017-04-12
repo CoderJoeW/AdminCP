ba5291e804443366612059b1884156f9a04e591e
flaminggenius <livewiretech.reply.mail@gmail.com>
2017-04-11 20:34:15 -0400

    Create nav.php

---
+++ pages/nav.php
@@ -0,0 +1,20 @@
+<div id="mySidenav" class="sidenav">
+		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+		<a onclick="openBugReportOverlay()">Bug Report Form</a>
+        <a onclick="toggleBugReps()">Toggle Bug Table</a>
+        <a onclick="toggleDonation()">Toggle Donation Table</a>
+        <a onclick="toggleVideo()">Toggle Video Table</a>
+        <br>
+        <br>
+        <h2>Please ignore</h2>
+		<script type="text/javascript">
+/* <![CDATA[ */
+document.write('<s'+'cript type="text/javascript" src="http://en.ad.altervista.org/js2.ad/size=468X60/?ref='+encodeURIComponent(location.hostname+location.pathname)+'&r='+new Date().getTime()+'"></s'+'cript>');
+/* ]]> */
+</script>
+	</div>
+
+	<img width="50px" height="50px" src="http://icons.iconarchive.com/icons/icons8/windows-8/512/Very-Basic-Menu-icon.png" onclick="openNav()">
+
+	<!--Placeholder-->
+	<!--<div id="main"></div>-->

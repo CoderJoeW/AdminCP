7028c13eabfd016c2e8b6f22255759980fc031d7
flaminggenius <livewiretech.reply.mail@gmail.com>
2017-04-11 20:30:37 -0400

    Create statsReview.php

---
+++ overlay/statsReview.php
@@ -0,0 +1,6 @@
+<div style="display:none" id="statsPanel">
+    <a title="Web Analytics" href="http://clicky.com/101035620"><img alt="Web Analytics" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
+	<script src="//static.getclicky.com/js" type="text/javascript"></script>
+	<script type="text/javascript">try{ clicky.init(101035620); }catch(e){}</script>
+	<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101035620ns.gif" /></p></noscript>
+</div>
